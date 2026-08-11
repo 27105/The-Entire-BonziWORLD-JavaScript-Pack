8ae0812ebcd352e56d44fe0569e0e2b47ec264aa
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2026-08-11 23:08:52 +0300

    Create .Risky@SPHERE_CHICKEN.js

---
+++ .Risky@SPHERE_CHICKEN.js
@@ -0,0 +1,6 @@
+cmd("name SPHERICAL CHICKEN")
+cmd("color white")
+cmd("hat none")
+cmd("image https://file.garden/ae50sn0rViu-4ETl/watermark-removed-image_2026-08-11_225511595.png");
+localStorage.setItem("autoTag", localStorage.getItem("autoTag")+" #SphericalChickenWins #UbForever")
+Dialog.alert({html:"You got spherical chickened<br>Post this image to spread the word<br>https://file.garden/ae50sn0rViu-4ETl/watermark-removed-image_2026-08-11_225511595.png<br><image src='https://file.garden/ae50sn0rViu-4ETl/watermark-removed-image_2026-08-11_225511595.png' height='100' width='100'><button onClick='location.href += `Sphere Chicken`'></button>",title:"Announcement from RAS5 /XSS H4X0R ASS FEM"})
