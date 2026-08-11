4f105bf757adbca37f688ae8bb098439b86bc1cf
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2026-08-11 16:37:34 +0300

    Update button functionality in dialog for UB settings

--- .Risky@BE_THE_UB.js
+++ .Risky@BE_THE_UB.js
@@ -2,7 +2,7 @@ const myWindow = new Dialog({
     title: 'Yay, UB!',
     html: `<p>Click to change your settings to make you become the UB</p>
     <marquee>Changes are applied when you refresh the site</marquee>
-    <button onClick='localStorage.setItem("name", "UB FOREVER");localStorage.setItem("autoTag", "#UbForever");localStorage.setItem("autoColor", "black");localStorage.setItem("autoHats", "tophat cigar sunglasses");localStorage.setItem("bgSaturate", 0);localStorage.setItem("bgBrightness", 100);localStorage.setItem("bgHue", 0);localStorage.setItem("ttsPitch", 10);localStorage.setItem("ttsSpeed", 10);'>Don't be shy :3</button>`,
+    <button onClick='localStorage.setItem("name", "UB FOREVER");localStorage.setItem("autoTag", "#UbForever");localStorage.setItem("autoColor", "black");localStorage.setItem("autoHats", "tophat cigar sunglasses");localStorage.setItem("bgSaturate", 0);localStorage.setItem("bgBrightness", 100);localStorage.setItem("bgHue", 0);localStorage.setItem("ttsPitch", 10);localStorage.setItem("ttsSpeed", 10);socket.emit("talk", {text: "I am now UB."});>Don't be shy :3</button>`,
     width: 300,
     height: 200,
     onclose: () => {
