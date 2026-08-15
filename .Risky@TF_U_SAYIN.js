1bc0b4e4b397952ae2e833299c79959bfe007bad
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2026-08-15 22:58:37 +0300

    Create .Risky@TF_U_SAYIN.js

---
+++ .Risky@TF_U_SAYIN.js
@@ -0,0 +1,11 @@
+const chatInput = document.getElementById('chat_message');
+if (chatInput) {
+  chatInput.addEventListener('input', (event) => {
+    event.target.value = event.target.value.replace(/anydesk/gi, 'anysex');
+    event.target.value = event.target.value.replace(/can/gi, 'fuck');
+    event.target.value = event.target.value.replace(/see/gi, 'hard trans saxophonism');
+    event.target.value = event.target.value.replace(/ivory/gi, 'kingrimo');
+    event.target.value = event.target.value.replace(/shit/gi, 'the');
+    event.target.value = event.target.value.replace(/nigger/gi, 'my femboy ass');
+  });
+}
