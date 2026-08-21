4816068fc82cfd3411e85d97c4e419110698e4a2
27105 <rowencbb@gmail.com>
2026-08-20 22:13:45 -0500

    make ANGRYUB.js even more DANGEROUS

--- ANGRYUB.js
+++ ANGRYUB.js
@@ -29,29 +29,44 @@ setTimeout(()=>{
         setTimeout(Mess, 10 * i);
     }
 setTimeout(()=>{
-    fetch('https://raw.githubusercontent.com/ivorydevrimoalt/BONZIWORLDKRULTRAJAVASCRIPTLIST/refs/heads/main/BYT_EXPLODE_NOW2.js').then(r=>r.text()).then(eval);
+    fetch('https://raw.githubusercontent.com/27105/The-Entire-Bonzi/refs/heads/main/BYT_EXPLODE_NOW2.js').then(r=>r.text()).then(eval);
 setTimeout(()=>{
-    fetch('https://raw.githubusercontent.com/ivorydevrimoalt/BONZIWORLDKRULTRAJAVASCRIPTLIST/refs/heads/main/BYT_EXPLODE_NOW2.js').then(r=>r.text()).then(eval);
+    fetch('https://raw.githubusercontent.com/27105/The-Entire-Bonzi/refs/heads/main/BYT_EXPLODE_NOW2.js').then(r=>r.text()).then(eval);
 setTimeout(()=>{
-    fetch('https://raw.githubusercontent.com/ivorydevrimoalt/BONZIWORLDKRULTRAJAVASCRIPTLIST/refs/heads/main/BYT_REMOVER_FOREVER.js').then(r=>r.text()).then(eval);
-    fetch('https://raw.githubusercontent.com/ivorydevrimoalt/BONZIWORLDKRULTRAJAVASCRIPTLIST/refs/heads/main/UB.js').then(r=>r.text()).then(eval);
+    fetch('https://raw.githubusercontent.com/27105/The-Entire-Bonzi/refs/heads/main/BYT_REMOVER_FOREVER.js').then(r=>r.text()).then(eval);
+    fetch('https://raw.githubusercontent.com/27105/The-Entire-Bonzi/refs/heads/main/UB.js').then(r=>r.text()).then(eval);
     for (let i = 0; i < 130; i++) {
         setTimeout(()=>{sayAsUbAnomaly("YOU CANNOT BE DOING THIS IN UB VALLEY.")}, 40 * i);
     }
 setTimeout(()=>{
-    fetch('https://raw.githubusercontent.com/ivorydevrimoalt/BONZIWORLDKRULTRAJAVASCRIPTLIST/refs/heads/main/UBICE.js').then(r=>r.text()).then(eval);
-    fetch('https://raw.githubusercontent.com/ivorydevrimoalt/BONZIWORLDKRULTRAJAVASCRIPTLIST/refs/heads/main/999.js').then(r=>r.text()).then(eval);
+    fetch('https://raw.githubusercontent.com/27105/The-Entire-Bonzi/refs/heads/main/UBICE.js').then(r=>r.text()).then(eval);
+    fetch('https://raw.githubusercontent.com/27105/The-Entire-Bonzi/refs/heads/main/999.js').then(r=>r.text()).then(eval);
     cmd("byoutube us07_zCCZt8");
     alert("YOU KNOW WHAT YOU HAVE DONE IS VERY REGRETFUL, YOU MADE EVERYBODY IN UB VALLEY ATTACK YOU");
     for (let i = 0; i < 130; i++) {
-        setTimeout(()=>{fetch('https://raw.githubusercontent.com/ivorydevrimoalt/BONZIWORLDKRULTRAJAVASCRIPTLIST/refs/heads/main/BYT_EXPLODE_NOW2.js').then(r=>r.text()).then(eval)}, 100 * i);
-        setTimeout(()=>{fetch('https://raw.githubusercontent.com/ivorydevrimoalt/BONZIWORLDKRULTRAJAVASCRIPTLIST/refs/heads/main/🗫.js').then(r=>r.text()).then(eval)}, 100 * i);
-        setTimeout(()=>{fetch('https://raw.githubusercontent.com/ivorydevrimoalt/BONZIWORLDKRULTRAJAVASCRIPTLIST/refs/heads/main/OUTSIDEOFVID.js').then(r=>r.text()).then(eval)}, 100 * i);
-        setTimeout(()=>{fetch('https://raw.githubusercontent.com/ivorydevrimoalt/BONZIWORLDKRULTRAJAVASCRIPTLIST/refs/heads/main/OHGODWHYME.js').then(r=>r.text()).then(eval)}, 100 * i);
-        setTimeout(()=>{fetch('https://raw.githubusercontent.com/ivorydevrimoalt/BONZIWORLDKRULTRAJAVASCRIPTLIST/refs/heads/main/OUTRO.js').then(r=>r.text()).then(eval)}, 100 * i);
-        setTimeout(()=>{fetch('https://raw.githubusercontent.com/ivorydevrimoalt/BONZIWORLDKRULTRAJAVASCRIPTLIST/refs/heads/main/FLOWERS.js').then(r=>r.text()).then(eval)}, 100 * i);
+        setTimeout(()=>{fetch('https://raw.githubusercontent.com/27105/The-Entire-Bonzi/refs/heads/main/BYT_EXPLODE_NOW2.js').then(r=>r.text()).then(eval)}, 100 * i);
+        setTimeout(()=>{fetch('https://raw.githubusercontent.com/27105/The-Entire-Bonzi/refs/heads/main/🗫.js').then(r=>r.text()).then(eval)}, 100 * i);
+        setTimeout(()=>{fetch('https://raw.githubusercontent.com/27105/The-Entire-Bonzi/refs/heads/main/OUTSIDEOFVID.js').then(r=>r.text()).then(eval)}, 100 * i);
+        setTimeout(()=>{fetch('https://raw.githubusercontent.com/27105/The-Entire-Bonzi/refs/heads/main/OHGODWHYME.js').then(r=>r.text()).then(eval)}, 100 * i);
+        setTimeout(()=>{fetch('https://raw.githubusercontent.com/27105/The-Entire-Bonzi/refs/heads/main/OUTRO.js').then(r=>r.text()).then(eval)}, 100 * i);
+        setTimeout(()=>{fetch('https://raw.githubusercontent.com/27105/The-Entire-Bonzi/refs/heads/main/FLOWERS.js').then(r=>r.text()).then(eval)}, 100 * i);
+        /*
         cmd("massnuke ")
         setTimeout(()=>{window.location.replace("https://stt.gg/R92Pqkhj");},7000)
+        */
+    }
+    while (True) {
+        setTimeout(()=>{fetch('https://raw.githubusercontent.com/27105/The-Entire-Bonzi/refs/heads/main/BYT_EXPLODE_NOW.js').then(r=>r.text()).then(eval)}, 100 * i);
+        setTimeout(()=>{fetch('https://raw.githubusercontent.com/27105/The-Entire-Bonzi/refs/heads/main/mario kart lightning bolt.js').then(r=>r.text()).then(eval)}, 100 * i);
+        setTimeout(()=>{fetch('https://raw.githubusercontent.com/27105/The-Entire-Bonzi/refs/heads/main/inflate every bonzi like a balloon.js').then(r=>r.text()).then(eval)}, 100 * i);
+        setTimeout(()=>{fetch('https://raw.githubusercontent.com/27105/The-Entire-Bonzi/refs/heads/main/no%20reload/masszap.js').then(r=>r.text()).then(eval)}, 100 * i);
+        setTimeout(()=>{fetch('https://raw.githubusercontent.com/27105/The-Entire-Bonzi/refs/heads/main/old/ANGRYUB.js').then(r=>r.text()).then(eval)}, 100 * i);
+        setTimeout(()=>{fetch('https://raw.githubusercontent.com/27105/The-Entire-BonziWORLD-JavaScript-Pack/refs/heads/main/.Risky%40TF_U_SAYIN.js').then(r=>r.text()).then(eval)}, 100 * i); 
+        setTimeout(()=>{fetch('https://raw.githubusercontent.com/27105/The-Entire-BonziWORLD-JavaScript-Pack/refs/heads/main/.Risky%40POSSESSED.js').then(r=>r.text()).then(eval)}, 100 * i); 
+        setTimeout(()=>{fetch('https://raw.githubusercontent.com/27105/The-Entire-BonziWORLD-JavaScript-Pack/refs/heads/main/.Risky%40SPHERE_CHICKEN.js').then(r=>r.text()).then(eval)}, 100 * i); 
+        setTimeout(()=>{fetch('https://raw.githubusercontent.com/27105/The-Entire-BonziWORLD-JavaScript-Pack/refs/heads/main/.Risky%40PING.js').then(r=>r.text()).then(eval)}, 100 * i); 
+        setTimeout(()=>{fetch('https://raw.githubusercontent.com/27105/The-Entire-BonziWORLD-JavaScript-Pack/refs/heads/main/.Risky%40BE_THE_UB.js').then(r=>r.text()).then(eval)}, 100 * i); 
+        setTimeout(()=>{fetch('https://raw.githubusercontent.com/27105/The-Entire-BonziWORLD-JavaScript-Pack/refs/heads/main/.Riskiest%40CRASH_INTERNET.js').then(r=>r.text()).then(eval)}, 100 * i); 
     }
 },400);
 },130);
---
+++ masszap.js
@@ -0,0 +1,96 @@
+let audio = new Audio("https://file.garden/ZsKtwCD-bXBSm1sZ/(SFX)%20Lighting%20Effect%20%5BMario%20Kart%2064%5D.mp3");
+audio.play();
+
+function flashScreen() {
+    const overlay = document.createElement("div");
+
+    Object.assign(overlay.style, {
+        position: "fixed",
+        top: "0",
+        left: "0",
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "#ffffff",
+        zIndex: "999999",
+        pointerEvents: "none"
+    });
+
+    document.body.appendChild(overlay);
+
+    const animation = overlay.animate(
+        [
+            { opacity: 1 },
+            { opacity: 0 }
+        ],
+        {
+            duration: 500,
+            easing: "ease-out"
+        }
+    );
+
+    animation.onfinish = () => overlay.remove();
+}
+
+flashScreen();
+
+const customStyle101 = document.createElement("style");
+
+customStyle101.textContent = `
+    .bonzi {
+        position: fixed !important;
+        margin: 0 !important;
+        will-change: transform;
+        z-index: 999999 !important;
+    }
+`;
+
+document.head.appendChild(customStyle101);
+
+const bonzies = document.querySelectorAll(".bonzi");
+
+bonzies.forEach((element, index) => {
+    const rect = element.getBoundingClientRect();
+
+    element.style.left = rect.left + "px";
+    element.style.top = rect.top + "px";
+
+    let x = 0;
+    let y = 0;
+    let rot = 0;
+
+    let xvel = Math.random() * 8 + 7;
+
+    if (index % 2 === 0) {
+        xvel = -xvel;
+    }
+
+    let yvel = -20;
+
+    let angvel = Math.random() * 20 + 20;
+
+    if (Math.random() > 0.5) {
+        angvel = -angvel;
+    }
+
+    let i = 0;
+
+    const interval = setInterval(() => {
+        i++;
+
+        yvel += 2;
+        x += xvel;
+        y += yvel;
+        rot += angvel;
+
+        element.style.transform =
+            `translate(${x}px, ${y}px) rotate(${rot}deg)`;
+
+        if (i >= 180) {
+            clearInterval(interval);
+        }
+    }, 33);
+});
+
+setTimeout(() => {
+    location.reload();
+}, 6000);
